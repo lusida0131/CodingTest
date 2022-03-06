a97daa5932b330b6e08b85222b55114e79cfd1cb
lusida0131 <ks12b9189@naver.com>
2022-03-06 21:51:37 +0900

    Day2

---
+++ src/Day2/BoardSum.java
@@ -0,0 +1,20 @@
+package Day2;
+import java.util.Scanner;
+
+public class BoardSum {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String st = sc.next();
+		String answer = "";
+		for(char x : st.toCharArray()) {
+			if(Character.isDigit(x)) {
+				answer += x;
+				
+			}
+		}
+		Integer.parseInt(answer);
+		System.out.println(answer);
+	}
+
+}
---
+++ src/Day2/MountainTop.java
@@ -0,0 +1,26 @@
+package Day2;
+import java.util.Scanner;
+
+public class MountainTop {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String st = sc.next();
+		String answer = "";
+		st = st + "";
+		int cnt = 1;
+		for(int i = 0; i < st.length() - 1; i++) {
+			if(st.charAt(i) == st.charAt(i+1)) {
+				cnt++;
+			}else {
+				answer += st.charAt(i);
+				if(cnt > 1) {
+					answer += String.valueOf(cnt);
+					cnt = 1;	
+				}
+			}
+		}
+		System.out.println(answer);
+	}
+
+}
