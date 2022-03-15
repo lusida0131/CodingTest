65f7fba0f844e8f5ce520b6d6f68119becb7a8ab
lusida0131 <ks12b9189@naver.com>
2022-03-15 23:10:09 +0900

    Day9

---
+++ src/Day9/LRU.java
@@ -0,0 +1,39 @@
+package Day9;
+
+import java.util.Scanner;
+
+public class LRU {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int [] arr = new int[m];
+		for(int i = 0; i < m; i++) {
+			arr[i] = sc.nextInt();
+		}
+		int [] cache = new int[n];
+		for(int x : arr) {
+			int pos = -1;
+			for(int i = 0; i < n; i++) {
+				if(x == cache[i]) {
+					pos = i;
+				}
+			}
+			if(pos == -1) {
+				for(int i = n - 1; i >= 1; i--) {
+					cache[i] = cache[i - 1];
+				}
+			}
+			else {
+				for(int i = pos; i >= 1; i--) {
+					cache[i] = cache[i - 1];
+				}
+			}
+			cache[0] = x;
+		}
+		for(int i = 0; i < n; i++) {
+			System.out.print(cache[i] + " ");
+		}
+	}
+}
