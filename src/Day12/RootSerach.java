d2f54260c0891be67b78743bf957bda829b609af
lusida0131 <ks12b9189@naver.com>
2022-03-28 23:59:51 +0900

    Day12, Day13

---
+++ src/Day12/RootSerach.java
@@ -0,0 +1,45 @@
+package Day12;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class RootSerach {
+	static int n, m, answer = 0;
+	static int [][] graph;
+	static int [] ch;
+	public void DFS(int v) {
+		if(v == n) {
+			answer++;
+		} else {
+			for(int i = 1; i< n; i++) {
+				if(graph[v][i] == 1 && ch[i] == 0) {
+					ch[i] = 1;
+					DFS(i);
+					ch[i] = 0;
+				}
+			}
+		}
+	}
+	public static void main(String[] args) throws IOException {
+		RootSerach T = new RootSerach();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m= Integer.parseInt(st.nextToken());
+		graph = new int [n + 1][n + 1];
+		ch = new int[n + 1];
+		for(int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			graph[a][b] = 1;
+		}
+		ch[1] = 1;
+		T.DFS(1);
+		System.out.println(answer);
+	}
+}
