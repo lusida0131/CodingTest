e2b438d9d91618635d4a3f129aaee74b79c4f5ee
lusida0131 <ks12b9189@naver.com>
2022-04-08 16:45:20 +0900

    Day1

---
+++ BaekJoon/Day1/IntCnt.java
@@ -0,0 +1,21 @@
+package Day1;
+import java.util.*;
+import java.io.*;
+
+public class IntCnt {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int pol = Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine());
+		int [] arr = new int[10];
+		String str = String.valueOf(pol);
+		for(int i = 0; i < str.length(); i++) {
+			arr[(str.charAt(i) - 48)]++;
+		}
+		for(int v : arr) {
+			System.out.println(v);
+		}
+	}
+
+}
