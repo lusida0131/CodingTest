d473c69bdf3ee7dbda48239213e23c6a8e8376fd
lusida0131 <ks12b9189@naver.com>
2022-04-13 00:23:58 +0900

    Day3

---
+++ BaekJoon/Day3/ASCII.java
@@ -0,0 +1,19 @@
+package Day3;
+import java.io.*;
+import java.util.*;
+public class ASCII {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String num = br.readLine();
+		char c = num.charAt(0);
+		if(Character.isAlphabetic(c)) {
+			int a = (int) c;
+			System.out.println(a);
+		}
+		else {
+			int a = (int) c;
+			System.out.println(a);
+		}
+	}
+}
