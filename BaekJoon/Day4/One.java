97ed98d2f9b2dfd30b111a28639236c1c81648fc
lusida0131 <ks12b9189@naver.com>
2022-04-13 22:12:44 +0900

    Day4

---
+++ BaekJoon/Day4/One.java
@@ -0,0 +1,26 @@
+package Day4;
+import java.io.*;
+import java.util.*;
+public class One {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long x = 1;
+		while(true) {
+			String st = br.readLine();
+			if(st == null) {
+				break;
+			}
+			int a = Integer.parseInt(st);
+			int val = 0;
+			for(int i = 1; i <= a; i++) {
+				val = val * 10 + 1;
+				val %= a;
+				if(val == 0) {
+					System.out.println(i);
+					break;
+				}
+			}
+		}
+	}
+}
